f3013a5ea8b6ed6a17468177abe467d50200dedf
Olena <poholska@gmail.com>
2025-01-26 18:19:01 -0600

    three taska

---
+++ js/task-3.js
@@ -0,0 +1,28 @@
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+
+  // Метод для зміни імені користувача
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  // Метод для оновлення ігрового часу
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  // Метод для отримання інформації про профіль
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  }
+};
+
+// Код для перевірки
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
